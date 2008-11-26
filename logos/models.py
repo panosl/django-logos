f31e8794e1137465e76bf0810e9eb844c05fa9f6
Panos Laganakos <panosl@panos-laganakoss-macbook.local>
2008-11-26 10:45:08 +0200

    updated el locale, added is_published manager.

--- logos/models.py
+++ logos/models.py
@@ -2,12 +2,21 @@ from django.db import models
 from django.utils.translation import gettext_lazy as _
 
 
+class PublicPostManager(models.Manager):
+	def get_query_set(self):
+		return super(PublicPostManager, self).get_query_set().filter(is_published=True)
+		
 
 class Post(models.Model):
 	title = models.CharField(_('title'), max_length=30)
 	slug = models.SlugField()
 	pub_date = models.DateTimeField(_('date published'))
 	body = models.TextField(_('body text'), blank=True)
+	is_published = models.BooleanField(_('it is published'), default=True,
+		help_text=_('Determines if it will be displayed at the website.'))
+
+	objects = models.Manager()
+	published = PublicPostManager()
 
 	class Meta:
 		ordering = ('-pub_date',)
