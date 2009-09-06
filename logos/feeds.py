0cfd3c2c7243f94534cbd7345ddffe23d3cbb856
Panos Laganakos <panosl@Menzoberanzan.local>
2009-09-06 17:37:25 +0300

    made feeds a bit more customizable.

--- logos/feeds.py
+++ logos/feeds.py
@@ -1,9 +1,10 @@
 from django.contrib.syndication.feeds import Feed
+from django.contrib.sites.models import Site
 from logos.models import Post
 
 
 class LatestPosts(Feed):
-	title = u'emporikos sullogos'	
+	title = Site.objects.get(pk=1).name
 	link = '/blog/'
 	description = ''
 
