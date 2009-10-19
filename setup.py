7e0b0149ed40d465a622dddd8926203d465f1aa5
Panos Laganakos <panosl@Menzoberanzan.local>
2009-10-19 13:34:48 +0300

    again setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+from distutils.core import setup
+
+
+setup(name='logos',
+	version='0.0.2',
+	description='Phaethon\'s Django based blog engine',
+	author='Panos Laganakos',
+	author_email='panos.laganakos@gmail.com',
+	packages=['logos'],
+	classifiers=['Development Status :: 4 - Beta',
+		'Environment :: Web Environment',
+		'Intended Audience :: Developers',
+		'License :: OSI Approved :: BSD License',
+		'Operating System :: OS Independent',
+		'Programming Language :: Python',
+		'Topic :: Utilities'],
+	)
+)
