a123848b9247b36b5d650a2054af8b32d921fd06
Panos Laganakos <panos.laganakos@gmail.com>
2014-06-08 18:19:31 +0300

    use ugettext_lazy.

--- logos/models.py
+++ logos/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.utils.translation import gettext_lazy as _
+from django.utils.translation import ugettext_lazy as _
 from django.contrib.comments.moderation import CommentModerator, moderator
 from logos.conf import settings
 if settings.USE_TAGS:
